e57b37b70fc5a59f6a81c63b1c93386115a18e35
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-27 00:10:03 +0530

    login user controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -86,4 +86,75 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "User Registerd Successfully"));
 });
 
-export { registerUser };
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generation refersh and access token"
+    );
+  }
+};
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "username or eamil is required!!");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(500, "User does not exist!!");
+  }
+
+  const passwordCheck = await user.isPasswordCorrect(password);
+
+  if (!passwordCheck) {
+    throw new ApiError(500, "Invalid user credential");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInuser = await user
+    .findById(user._id)
+    .select("-password -refreshToken");
+
+  //sending cookies
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(201)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInuser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+export { registerUser, loginUser };
